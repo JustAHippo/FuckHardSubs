0cbad01f3d6e8e768d354ebe4a128ead6e006133
JustAHippo <82006314+JustAHippo@users.noreply.github.com>
2022-06-18 23:29:56 -0400

    better filters plus recognition
    Will add text similarity and other stuff soon including SRT output

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ pytesseract.pytesseract.tesseract_cmd = 'TESSERACT PATH\\tesseract.exe'
 vidcap = cv2.VideoCapture('PATH TO VIDEO')
 success,image = vidcap.read()
 c = 0
+lastRet = ""
 while success:
   #cv2.destroyAllWindows()
   success,image = vidcap.read()
@@ -18,16 +19,18 @@ while success:
   # img[y:y+h, x:x+w]
   #img = cv2.convertScaleAbs(img, alpha=3, beta=100)
 
-  cropped_image = img[int(1/1.2*h):h, int(1/4 * w):int(2/2.6 * w)]
+  cropped_image = img[int(1/1.2*h):h, int(1/5 * w):int(2/2.3 * w)]
   gray_img_copy = np.copy(cropped_image)
-  gray_img_copy[gray_img_copy[:, :] < 250] = 0
+  gray_img_copy[gray_img_copy[:, :] < 252] = 0
   cv2.imshow('current frame', gray_img_copy, )
   cv2.waitKey(1)
 
 
-
-  print(pytesseract.image_to_string(gray_img_copy, lang='eng', config="--psm 6 --oem 1" ))
-  print(c)
+  ret = pytesseract.image_to_string(gray_img_copy, lang='eng', config="--psm 6 --oem 1" )
+  if ret != "" and ret != lastRet:
+    lastRet = ret
+    ret.replace("|", "I")
+    print(c, ret)
   # time.sleep(0.25)
   c += 1
 
